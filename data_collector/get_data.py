f9ba630c6973b8f5662acefede1934a82abb9f24
fe-bern <berner.fe@gmail.com>
2020-07-03 10:39:23 +0200

    cleaned up

--- data_collector/get_data.py
+++ data_collector/get_data.py
@@ -1,39 +1,38 @@
-'''Collecting Data and storing in as DF in MySQL'''
+'''Collecting Data and storing as DF in MySQL'''
 
-import gzip
-import pandas as pd
-import requests
 from datetime import date
 import urllib.request
+import gzip
 
-aws_url = "https://s3-eu-west-1.amazonaws.com/public.bitmex.com/data/quote/"
-file_ending = ".csv.gz"
+AWS_URL = "https://s3-eu-west-1.amazonaws.com/public.bitmex.com/data/quote/"
+FILE_ENDING = ".csv.gz"
 
 def get_recent_url():
+    '''Get newest data, data has a delay of 2 days.'''
     today = date.today().strftime("%Y%M%D").replace("/", "")
     new_today = today[:3] + today[5:10]
-    new_today = str(int(new_string) - 2)
-    url_data = aws_url + new_today + file_ending
+    new_today = str(int(new_today) - 2)
+    url_data = AWS_URL + new_today + FILE_ENDING
     return url_data
 
 def download_file(url):
-   out_file = '/Users/felixberner/Desktop/finalproject/data/out_file.csv'
+    '''Downloads the gz file and write the content to a csv file.'''
+    out_file = '/Users/felixberner/Desktop/finalproject/data/out_file.csv'
+    # Download archive
+    try:
+        # Read the file inside the .gz archive located at url
+        with urllib.request.urlopen(url) as response:
+            with gzip.GzipFile(fileobj=response) as uncompressed:
+                file_content = uncompressed.read()
 
-   # Download archive
-   try:
-      # Read the file inside the .gz archive located at url
-      with urllib.request.urlopen(url) as response:
-         with gzip.GzipFile(fileobj=response) as uncompressed:
-            file_content = uncompressed.read()
+        # write to file in binary mode 'wb'
+        with open(out_file, 'wb') as f:
+            f.write(file_content)
+            return 0
 
-      # write to file in binary mode 'wb'
-      with open(out_file, 'wb') as f:
-         f.write(file_content)
-         return 0
-
-   except Exception as e:
-      print(e)
-      return 1
+    except Exception as e:
+        print(e)
+        return 1
 
 get_recent_url()
 download_file(url_data)
