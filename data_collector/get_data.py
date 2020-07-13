74416f899164eab19a15fa331e45b981fc6f7d2c
fe-bern <berner.fe@gmail.com>
2020-07-13 17:57:10 +0200

    added etl skeleton

--- data_collector/get_data.py
+++ data_collector/get_data.py
@@ -3,9 +3,18 @@
 from datetime import date
 import urllib.request
 import gzip
+import pandas as pd
+from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+import logging
+import time
+import mysql.connector
+
 
 AWS_URL = "https://s3-eu-west-1.amazonaws.com/public.bitmex.com/data/quote/"
 FILE_ENDING = ".csv.gz"
+OUT_FILE = 'out_file.csv'
 
 def get_recent_url():
     '''Get newest data, data has a delay of 2 days.'''
@@ -17,7 +26,6 @@ def get_recent_url():
 
 def download_file(url):
     '''Downloads the gz file and write the content to a csv file.'''
-    out_file = '/Users/felixberner/Desktop/finalproject/data/out_file.csv'
     # Download archive
     try:
         # Read the file inside the .gz archive located at url
@@ -26,7 +34,7 @@ def download_file(url):
                 file_content = uncompressed.read()
 
         # write to file in binary mode 'wb'
-        with open(out_file, 'wb') as f:
+        with open(OUT_FILE, 'wb') as f:
             f.write(file_content)
             return 0
 
@@ -34,5 +42,25 @@ def download_file(url):
         print(e)
         return 1
 
-get_recent_url()
+def storing_in_mysql(out_file):
+    '''Transforms the csv file to a DF and reads at in MySQL'''
+    #setting up connection to MySQL
+    load_dotenv()
+    mysql_user = os.getenv('MYSQL_USER')
+    mysql_password= os.getenv('MYSQL_PASSWORD')
+    mysql_host = os.getenv('MYSQL_HOST')
+    mysql_port = os.getenv('MYSQL_PORT')
+    conns = f"mysql+pymysql://{mysql_user}:{mysql_password}@{mysql_host}:{mysql_port}/bitmex_data"
+    engine = create_engine(conns, encoding="latin1", echo=True)
+
+    #reading in file and store it in MySQL
+    data = pd.read_csv(out_file)
+    data.to_sql('table_bitmex', con=engine, if_exists='replace')
+
+
+logging.critical('starting process')
 download_file(get_recent_url())
+logging.critical('downloading subprocess finished')
+#storing_in_mysql(OUT_FILE) commented out because MySQL local gets an OOM Error otherwise
+logging.critical('storing subprocess finished')
+logging.critical('ending process')
